ec757636a6ebf9a7fbe35aaec50b08efa4487996
wembley-fraggle <roman.holzner@students.fhnw.ch>
2015-03-23 11:44:32 -0700

    Minimum implemented. Now there is no more the mood to do more

---
+++ source/DotNetBay.WebApp/Controllers/BidsController.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using DotNetBay.Core;
+using DotNetBay.Data.EF;
+using DotNetBay.Interfaces;
+using DotNetBay.Model;
+using DotNetBay.WebApp.Models;
+
+namespace DotNetBay.WebApp.Controllers
+{
+    public class BidsController : Controller
+    {
+        private IMainRepository repo;
+        private AuctionService service;
+        private IMemberService memberService;
+
+        public BidsController()
+        {
+            this.repo = new EFMainRepository();
+            this.memberService = new SimpleMemberService(this.repo); 
+            this.service = new AuctionService
+                (
+                this.repo,
+                this.memberService
+            );
+        }
+
+        // GET: Bids
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: Bids/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: Bids/Create
+        public ActionResult Create(int id)
+        {
+            Auction auction =  this.service.GetById(id);
+            PlaceBidViewModel viewModel = new PlaceBidViewModel();
+            viewModel.AuctionId = id;
+            viewModel.Title = auction.Title;
+            viewModel.Description = auction.Description;
+            viewModel.StartPrice = auction.StartPrice;
+            viewModel.CurrentPrice = auction.CurrentPrice;
+            viewModel.CurrentBid = auction.CurrentPrice + 1;
+            return View(viewModel);
+        }
+
+        // POST: Bids/Create
+        [HttpPost]
+        public ActionResult Create(PlaceBidViewModel viewModel)
+        {
+            try
+            {
+                Member bidder = memberService.GetCurrentMember();
+                Auction auction = this.service.GetById(viewModel.AuctionId);
+                auction.ActiveBid = this.service.PlaceBid(bidder, auction, viewModel.CurrentBid);
+                auction.Bids.Add(auction.ActiveBid);
+                auction.CurrentPrice = auction.ActiveBid.Amount;
+                this.service.Save(auction);
+                return RedirectToAction("Index","Auctions");
+            }
+            catch
+            {
+                return View(viewModel);
+            }
+        }
+    }
+}
---
+++ source/DotNetBay.WebApp/Models/PlaceBidViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using DotNetBay.Model;
+
+namespace DotNetBay.WebApp.Models
+{
+    public class PlaceBidViewModel
+    {
+        public int AuctionId { get; set; }
+        public string Title{ get; set; }
+        public string Description { get; set; }
+        public double StartPrice { get; set; }
+        public double CurrentPrice { get; set; }
+        public double CurrentBid { get; set; }
+    }
+}
