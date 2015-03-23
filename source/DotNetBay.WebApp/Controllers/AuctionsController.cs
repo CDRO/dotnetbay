5948264bf1697e1f50b50ad4e4f97b2558c50c50
wembley-fraggle <roman.holzner@students.fhnw.ch>
2015-03-23 01:33:49 -0700

    added generated auction view page

---
+++ source/DotNetBay.WebApp/Controllers/AuctionsController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace DotNetBay.WebApp.Controllers
+{
+    public class AuctionsController : ApiController
+    {
+        // GET: api/Auctions
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET: api/Auctions/5
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST: api/Auctions
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT: api/Auctions/5
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE: api/Auctions/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
