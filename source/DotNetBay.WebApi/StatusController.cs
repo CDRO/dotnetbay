4f5429e4f5c3516e2283098cd5821b0ddd0b5b7b
wembley-fraggle <roman.holzner@students.fhnw.ch>
2015-05-19 07:20:29 +0200

    First Controller with route and status call

--- source/DotNetBay.WebApi/StatusController.cs
+++ source/DotNetBay.WebApi/StatusController.cs
@@ -3,10 +3,19 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Web.Http;
 
 namespace DotNetBay.WebApi
 {
-    class StatusController : ApiController
+    [Route("api")]
+    public class StatusController : ApiController
     {
+        [HttpGet]
+        [Route("status")]
+        public IHttpActionResult AreYouFine()
+        {
+            return this.Ok("I am fine");
+        }
+
     }
 }
